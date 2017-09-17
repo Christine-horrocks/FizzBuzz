61b3a38d237215068781956e2321c12986325f69
Christine <splogy2001@hotmail.com>
2017-09-17 18:10:57 +0100

    Finished FizzBuzz

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,5 +1,13 @@
 #This is the FizzBuzz challenge
 
 def fizzbuzz(number)
-  "fizz"
+  if number %5 == 0 && number %3 == 0
+    "fizzbuzz"
+  elsif number %5 == 0
+    "buzz"
+  elsif number %3 == 0
+    "fizz"
+  else
+    number
+  end 
 end
--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,8 +1,18 @@
-require_relative '../lib/fizzbuzz'
+require 'fizzbuzz'
 
 
 describe 'fizzbuzz' do
-  it 'returns "fizz" when passed 3' do
+  it 'returns "fizz" when passed a multiple of 3. Example 3' do
     expect(fizzbuzz(3)).to eq 'fizz'
   end
+  it 'returns "buzz" when passed a multiple of 5. Example 5' do
+    expect(fizzbuzz(5)).to eq 'buzz'
+  end
+  it 'returns "fizzbuzz" when passed a multiple of 3 and 5. Example = 15' do
+    expect(fizzbuzz(15)).to eq 'fizzbuzz'
+  end
+  it 'returns number when passed a number not a multiple of 3 or 5' do
+    expect(fizzbuzz(4)).to eq 4
+  end
+
 end
